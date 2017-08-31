29b5308e90fb0933d1a8db025ea04ce29543e543
JacobAhlberg <jacahlberg@hotmail.com>
2017-08-31 19:33:26 +0200

    transer data through segues using models

---
+++ Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Jacob Ahlberg on 2017-08-31.
+//  Copyright © 2017 Jacob Ahlberg. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+}
